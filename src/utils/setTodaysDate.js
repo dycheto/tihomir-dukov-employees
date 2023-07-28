1cc0ada7f6affdfa1adb199f0e717609f7725e01
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-07-28 11:09:58 +0300

    Create setTodaysDate.js

---
+++ src/utils/setTodaysDate.js
@@ -0,0 +1,13 @@
+// when DateTo is null, this method returns todays date formated yyyy-mm-dd
+
+export function setTodaysDate(date = new Date()) {
+    let year = date.toLocaleString('default', { year: 'numeric' }).slice(0, 4);
+
+    const month = date.toLocaleString('default',
+        {
+            month: '2-digit',
+        });
+    const day = date.toLocaleString('default', { day: '2-digit' });
+
+    return [year, month, day].join('-');
+}
