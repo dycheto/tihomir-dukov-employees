013f5288ff4edd0c785fba1dbc4e6ffc1bbd57ce
Tihomir Dukov <106734442+Dycheto@users.noreply.github.com>
2023-07-28 11:10:04 +0300

    Update sortByProjectId.js

--- src/utils/sortByProjectId.js
+++ src/utils/sortByProjectId.js
@@ -1,4 +1,5 @@
-import { getEmployeePairs } from './getImployeesPairs'
+import { getEmployeePairs } from './getEmployeesPairs';
+import { calculateDaysWorkedTogether } from './calculateDaysWorkedTogether';
 
 export function sortByProjectId(employeesData) {
 
@@ -7,31 +8,11 @@ export function sortByProjectId(employeesData) {
 
   let result = [];
 
-  for (const projectEmployees of Object.values(employeePairs)) {
-    if (projectEmployees.length !== 2) { continue; }
-    // set the start date and the end date of both employees 
-    const startDateA = new Date(projectEmployees[0].DateFrom);
-    const endDateA = new Date(projectEmployees[0].DateTo);
-    const startDateB = new Date(projectEmployees[1].DateFrom);
-    const endDateB = new Date(projectEmployees[1].DateTo);
+  for (const signlePair of Object.values(employeePairs)) {
+    if (signlePair.length < 2) { continue; }
 
-    //in the next lines we set the start date and the end date of the pair common work
-    const overlapStartDate = new Date(Math.max(startDateA, startDateB));
-    const overlapEndDate = new Date(Math.min(endDateA, endDateB));
+    let pair = calculateDaysWorkedTogether(signlePair);
 
-    // we calculate the total days of working together
-    const durationInMs = overlapEndDate - overlapStartDate;
-    const durationInDays = durationInMs / (1000 * 60 * 60 * 24);
-
-
-    let pair = {
-      firstEmployeeId: projectEmployees[0].EmpID,
-      secondEmployeeId: projectEmployees[1].EmpID,
-      projectId: projectEmployees[0].ProjectID,
-      duration: durationInDays
-    };
-
-    //push pair in array
     result.push(pair)
   }
 
